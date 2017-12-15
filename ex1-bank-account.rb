4f9733c596af27402145b397b70be7c3110637e1
jeffrothwell <rothwelljeff@gmail.com>
2017-12-15 15:01:15 -0500

    complete bank account exercise

--- ex1-bank-account.rb
+++ ex1-bank-account.rb
@@ -19,14 +19,29 @@ class BankAccount
   def withdrawl(amount)
     @balance -= amount
   end
+
+  #a method to apply interest to the
+  def gain_interest
+    @balance = @balance * (1 + @interest_rate)
+  end
+
 end
 
+# make an object called jeff_account with a class of BankAccount
+# in my class definition I initialize it with zero balance and r=3%
 jeff_account = BankAccount.new
 
+# put 100 bucks in jeff_account
 jeff_account.deposit(100)
 
 puts "Jeff has $#{jeff_account.balance_check}"
 
+# take 100 bucks out
 jeff_account.withdrawl(25)
 
 puts "After taking out $25, Jeff has $#{jeff_account.balance_check}"
+
+# apply some interest to the account
+jeff_account.gain_interest
+
+puts "Interest has accrued on Jeff's account, he now has $#{jeff_account.balance_check}"
