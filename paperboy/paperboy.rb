039245bab6cdb9d0dd6a0bbeb45a7dbd721ab900
jeffrothwell <rothwelljeff@gmail.com>
2017-12-15 22:09:28 -0500

    write quota and deliver instance methods, successfully initialize a paperboy

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -1,3 +1,31 @@
 class PaperBoy
-  
+
+  def initialize (name)
+    @name = name
+    @experience = 0
+    @earnings = 0
+  end
+
+  def quota
+    50.0 + (@experience/2.0)
+  end
+
+  # I know for the deliver method the problem said assume end_address > start_address
+  # using the absolute method removes this necessity
+
+  def deliver(start_address, end_address)
+    if (start_address - end_address).abs == quota
+      @earnings += (start_address - end_address).abs * 0.25
+    elsif (start_address - end_address).abs < quota
+      @earnings += (start_address - end_address).abs * 0.25
+      @earnings -= 2.0
+    else  #for those over quota, give the standard .25 for all, plust th extra .25 for all above quota
+      @earnings += (start_address - end_address).abs * 0.25
+      @earnings += ((start_address - end_address).abs - quota) * 0.25
+    end
+  end
+
 end
+
+adam = PaperBoy.new("Adam")
+p adam
